33b1987659951900d4122f802077cbd4846bfd8d
mihaidxc <mihairazvantudorache1998@gmail.com>
2021-04-01 18:39:53 +0300

    Initial Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import TodoList from "./components/TodoList";
+import EditTodo from "./components/EditTodo";
+import CreateTodo from "./components/CreateTodo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className='navbar bg-light navbar-expand-lg navbar0light'>
+        <ul className='navbar-nav mr-auto'>
+          <li className='navbar-item'>
+            <Link to='/' className='navbar-link'>
+              Todos
+            </Link>
+          </li>
+          <li className='navbar-item'>
+            <Link to='/create' className='navbar-link'>
+              Create Todo
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path='/' component={TodoList} />
+        <Route exact path='/edit/:id' component={EditTodo} />
+        <Route exact path='/create' component={CreateTodo} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/EditTodo.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import TodoForm from "./TodoForm";
+
+function EditTodo() {
+  const [todo, setTodo] = useState();
+
+  useEffect(() => {
+    setTodo({
+      text: "foo",
+    });
+  }, []);
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
+
+  return todo ? (
+    <div className='container'>
+      <div className='mt-3'>
+        <h3>Edit Todo Item</h3>
+        <TodoForm todo={todo} onSubmit={onSubmit} />
+      </div>
+    </div>
+  ) : (
+    <div>Loading</div>
+  );
+}
+
+export default EditTodo;
